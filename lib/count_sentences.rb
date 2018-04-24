d85d0317487bf77e9b764db0b0f1c489d017cc30
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-24 17:10:40 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,6 @@ require 'pry'
 
 class String
 
-    #@sentence_count = []
-
   def sentence?
     if self.end_with?(".")
       true
@@ -32,7 +30,7 @@ class String
     if self.size == 0
       0
     else
-      self.split("?").size + self.split(".").size + self.split("!").size - 2
+      self.split(/[?.!]|[?.!]*/).size
     end
   end
 #      "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
