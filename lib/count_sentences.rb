7dda071e5e3246666e902608af0197e0023825ec
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-24 17:10:24 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,39 @@ require 'pry'
 
 class String
 
+    #@sentence_count = []
+
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    if self.size == 0
+      0
+    else
+      self.split("?").size + self.split(".").size + self.split("!").size - 2
+    end
   end
+#      "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+#  self.join
 end
